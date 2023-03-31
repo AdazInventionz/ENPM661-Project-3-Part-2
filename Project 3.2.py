4653be04a3a91004d4487509b301ad06d2b815a2
AdazInventionz <56547728+AdazInventionz@users.noreply.github.com>
2023-03-30 20:42:52 -0400

    Obstacles, Distance Borders, and Robot Clearance
    Added borders and radial clearance

--- Project 3.2.py
+++ Project 3.2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[10]:
 
 
 #Imported libraries
@@ -15,7 +15,7 @@ import math
 
 
 #Sets up arena with obstacles
-def setup():
+def setup(s, r):
 
     global arena
     
@@ -24,21 +24,21 @@ def setup():
     gray = (177, 177, 177)
     darkGray = (104, 104, 104)
     
-#     #Draw Radial Clearance
-#     for x in range(0, 600):
+    #Draw Radial Clearance
+    for x in range(0, 600):
 
-#         for y in range(0, 200):
+        for y in range(0, 200):
         
-#             if checkClearance(x, y, 4):
-#                 arena[y, x] = darkGray
+            if checkClearance(x, y, s, r):
+                arena[y, x] = darkGray
     
-#     #Draw Obstacle Borders
-#     for x in range(0, 600):
+    #Draw Obstacle Borders
+    for x in range(0, 600):
 
-#         for y in range(0, 200):
+        for y in range(0, 200):
         
-#             if checkBorder(x, y):
-#                 arena[y, x] = gray
+            if checkBorder(x, y, s):
+                arena[y, x] = gray
     
     #Draw Obstacles
     for x in range(0, 600):
@@ -64,108 +64,68 @@ def checkObstacle(x, y):
             return True
         
     #Circle
-    if (x - 400) * (x - 400) + (y - 90) * (y - 90) <= 2500:
+    if (x - 400) * (x - 400) + (y - 90) * (y - 90) <= 50*50:
         return True
-    
-    #Pentagon (Left Half)
-#     if x >= 235 and x <= 300:
-        
-#         if (y >= (-38/65)*x + (2930/13)) and (y <= (38/65)*x + (320/13)):
-#             return True
-    
-#     #Pentagon (Right Half)
-#     if x >= 300 and x <= 366:
-        
-#         if (y >= (38/65)*x + (-1630/13)) and (y <= (-38/65)*x + (4880/13)):
-#             return True
-    
-#     #Triangle
-#     if x >= 460 and x <= 510:
-        
-#         if (y >= 2*x - 895) and (y <= -2*x + 1145):
-#             return True
         
     return False
   
 #Checks to see if a point is within the border of an obstacle
-def checkBorder(x, y):
+def checkBorder(x, y, s):
     
-    triHeight = int(round(5/math.cos(math.radians(63.4))))
-    hexHeight = int(round(5/math.cos(math.radians(30.3))))
-    
-    #Both Rectangles
-    if x >= 100 - 5 and x <= 150 + 5:
+    #Left Rectangle
+    if x >= 150 - s and x < 165 + s:
         
-        if y < 100 + 5 or y >= 150 - 5:
+        if y < 125 + s and y >= 0:
             return True
     
-    #Pentagon (Left Half)
-    if x >= 235 - 5 and x <= 300:
+    #Right Rectangle
+    if x >= 250 - s and x < 265 + s:
         
-        if (y >= (-38/65)*x + (2930/13) - hexHeight) and (y <= (38/65)*x + (320/13) + hexHeight):
+        if y < 200 + s and y >= 75 - s:
             return True
-    
-    #Pentagon (Right Half)
-    if x >= 300 and x <= 366 + 5:
         
-        if (y >= (38/65)*x + (-1630/13) - hexHeight) and (y <= (-38/65)*x + (4880/13) + hexHeight):
-            return True
-    
-    #Triangle
-    if x >= 460 - 5 and x <= 510 + 5:
-        
-        if (y >= 2*x - 895 - triHeight) and (y <= -2*x + 1145 + triHeight) and (y >= 25 - 5) and (y <= 225 + 5):
-            return True
+    #Circle
+    if (x - 400) * (x - 400) + (y - 90) * (y - 90) <= (50 + s) * (50 + s):
+        return True
         
     return False
 
 #Checks to see if a point is within radial clearance of a border
-def checkClearance(x, y, r):
+def checkClearance(x, y, s, r):
     
     rr = r - 1
     
     if rr == 0:
         return False
     
-    triHeight = int(round((5 + rr)/math.cos(math.radians(63.4))))
-    hexHeight = int(round((5 + rr)/math.cos(math.radians(30.3))))
-    
-    #Both Rectangles
-    if x >= 100 - 5 - rr and x <= 150 + 5 + rr:
+    #Left Rectangle
+    if x >= 150 - s - rr and x < 165 + s + rr:
         
-        if y < 100 + 5 + rr or y >= 150 - 5 - rr:
+        if y < 125 + s + rr and y >= 0:
             return True
     
-    #Pentagon (Left Half)
-    if x >= 235 - 5 - rr and x <= 300:
+    #Right Rectangle
+    if x >= 250 - s - rr and x < 265 + s + rr:
         
-        if (y >= (-38/65)*x + (2930/13) - hexHeight) and (y <= (38/65)*x + (320/13) + hexHeight):
+        if y < 200 + s + rr and y >= 75 - s - rr:
             return True
-    
-    #Pentagon (Right Half)
-    if x >= 300 and x <= 366 + 5 + rr:
         
-        if (y >= (38/65)*x + (-1630/13) - hexHeight) and (y <= (-38/65)*x + (4880/13) + hexHeight):
-            return True
-    
-    #Triangle
-    if x >= 460 - 5 - rr and x <= 510 + 5 + rr:
-        
-        if (y >= 2*x - 895 - triHeight) and (y <= -2*x + 1145 + triHeight) and (y >= 25 - 5 - rr) and (y <= 225 + 5 + rr):
-            return True
+    #Circle
+    if (x - 400) * (x - 400) + (y - 90) * (y - 90) <= (50 + s + rr) * (50 + s + rr):
+        return True
         
     return False
 
 #Checks to see if a point is valid (by checking obstacle, border, and clearance, as well as making sure the point is within arena bounds)
-def checkValid(x, y, r):
+def checkValid(x, y, s, r):
     
     if checkObstacle(x, y):
         return False
     
-    if checkBorder(x, y):
+    if checkBorder(x, y, s):
         return False
     
-    if checkClearance(x, y, r):
+    if checkClearance(x, y, s, r):
         return False
     
     if (x < 0 or x >= 600 or y < 0 or y >= 250):
@@ -180,9 +140,7 @@ def checkValid(x, y, r):
 #Main Code
 arena = np.zeros((200, 600, 3), dtype = "uint8")
 
-
-
-setup()
+setup(5, 10) #Change the values to change border size and robot radius, respectively
 cv.imshow("Arena", arena)
 cv.waitKey()
 cv.destroyAllWindows()
